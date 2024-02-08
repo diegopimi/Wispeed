ad8b0369e01450c161a638f36bc9a4ada23ee6ce
diegopimi <92610979+diegopimi@users.noreply.github.com>
2024-02-08 17:04:23 -0600

    Reorganized backend, added app, and index.htm |||| Comments will be added in the future

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re
 import datetime
 from pymongo import MongoClient
 import crudFunc
+from functionalities import repeat_function, main_func
 
 # Replace 'localhost' with the actual hostname or IP address of your MongoDB server.
 # If your MongoDB server is running on the default port (27017), you don't need to specify it.
@@ -11,31 +12,5 @@ client = MongoClient('localhost', 27017)
 # Replace 'your_database_name' with the name of your existing database.
 db = client['DBPimi']
 
-# Define the command to run
-command = ["speedtest-cli", "--secure"]
-
-# Run the command and capture the output
-result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
-
-# Print the command output
-print("=======Performing WIFI Test=======")
-
-# print(result.stdout)
-result = result.stdout
-
-lines = result.split('\n')
-
-download_speed_line = [line for line in lines if 'Download:' in line]
-upload_speed_line = [line for line in lines if 'Upload:' in line]
-
-download_speed = download_speed_line[0].split('Download: ')[1].strip()
-upload_speed = upload_speed_line[0].split('Upload: ')[1].strip()
-
-print("Download Speed:", download_speed)
-print("Upload Speed:", upload_speed)
-date_r = datetime.date.today()
-date_r = date_r.strftime("%Y-%m-%d")
-time_r = datetime.datetime.now()
-time_r = time_r.strftime("%H:%M:%S")
-crudFunc.addReading(download_speed, upload_speed, date_r, time_r)
-
+# Call the repeat_function with my_function and a frequency of 2 seconds
+repeat_function(main_func, 1)
