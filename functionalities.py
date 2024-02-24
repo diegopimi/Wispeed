145b92e72cf9be1bfe1a6c1e02270911c691af7a
diegopimi <92610979+diegopimi@users.noreply.github.com>
2024-02-24 09:59:16 -0600

    Added periodic functionality. This will need to be interfaced to include proper inputs sfrom the user.

--- functionalities.py
+++ functionalities.py
@@ -12,7 +12,7 @@ global_optn = "--secure"
 def repeat_function(func, interval):
     count = 0
     while (count<interval):
-        func()  # Call the specified function
+        func()                # Call the specified function
         time.sleep(interval)  # Wait for the specified interval
         count=count+1
 
@@ -31,9 +31,9 @@ def main_func():
 
     lines = result.split('\n')
 
+    # parse command output
     download_speed_line = [line for line in lines if 'Download:' in line]
     upload_speed_line = [line for line in lines if 'Upload:' in line]
-
     download_speed = download_speed_line[0].split('Download: ')[1].strip()
     upload_speed = upload_speed_line[0].split('Upload: ')[1].strip()
 
@@ -44,3 +44,11 @@ def main_func():
     time_r = datetime.datetime.now()
     time_r = time_r.strftime("%H:%M:%S")
     crudFunc.addReading(download_speed, upload_speed, date_r, time_r)
+
+def periodic_reading(frequency, max):
+    # Frequency shall be read in minutes or hours.
+    counter = 0
+    while counter < max:
+        main_func()  # Call the main_func function
+        time.sleep(frequency)  # Convert frequency to minutes
+        counter += 1
