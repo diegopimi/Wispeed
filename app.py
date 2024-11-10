d44b19d155bc5fc3832d8f62107787e59259885b
diegopimi <92610979+diegopimi@users.noreply.github.com>
2024-11-10 10:22:11 -0600

    read and create file if needed, update dependencies

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
 import subprocess
 import sched
 import time
-from datetime import datetime
-from functionalities import return_reading, return_by_download, return_by_upload, return_all, seconds_to_minutes, reading_at, scheduler
 from WiGraph.plot import graph_download, graph_upload
 import plotly.graph_objs as go
 import plotly.io as pio
+from datetime import datetime
+from file_manager import does_path_exist, create_path, log_file_path
+from requester import return_reading, return_by_download, return_by_upload, return_all, seconds_to_minutes, reading_at, scheduler
+import json
+
+def read_file():
+    with open(log_file_path, "r") as file:
+        data = json.load(file)
+    return data
+
+if does_path_exist() != True:
+    print("Creating File")
+    create_path()
+else:
+    print("File already exists")
 
 scheduler = sched.scheduler(time.time, time.sleep)
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/DBPimi'
-mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    data = mongo.db.WiSpeed.find()
+    print("Index has been called")
+    try:
+        data = read_file() 
+    except FileNotFoundError:
+        create_path()
+        data = read_file()
     return render_template('index.html', data=data)
 
 @app.route('/run_main', methods=['POST'])
@@ -24,7 +39,7 @@ def run_main():
     try:
         subprocess.run(["python", "main.py"], check=True)
     except subprocess.CalledProcessError as e:
-        print("Error running main.py:", e)
+        print("app.py Error running main.py:", e)
     return redirect(url_for('index'))
 
 @app.route('/run_periodical', methods=['POST'])
@@ -48,8 +63,8 @@ def run_periodical():
 @app.route('/return_dated', methods=['POST'])
 def return_dated():
     try:    
-        date = str(request.form['date'])          # Get date from the form
-        readings=return_reading(date)              # Call the periodic_reading function with the values
+        date = str(request.form['date'])          
+        readings=return_reading(date)              
         return render_template('index.html', data=readings)
     except subprocess.CalledProcessError as e:
         print("Error running request:", e)
@@ -58,7 +73,7 @@ def return_dated():
 @app.route('/return_all_readings', methods=['POST'])
 def return_all_readings():
     try:    
-        readings=return_all()          # Call the periodic_reading function with the values
+        readings=return_all()         
         return render_template('index.html', data=readings)
     except subprocess.CalledProcessError as e:
         print("Error running request:", e)
@@ -67,7 +82,7 @@ def return_all_readings():
 @app.route('/view_by_download', methods=['POST'])
 def view_by_download():
     try:    
-        readings=return_by_download()          # Call the periodic_reading function with the values
+        readings=return_by_download()         
         return render_template('index.html', data=readings)
     except subprocess.CalledProcessError as e:
         print("Error running request:", e)
@@ -76,7 +91,7 @@ def view_by_download():
 @app.route('/view_by_upload', methods=['POST'])
 def view_by_upload():
     try:    
-        readings=return_by_upload()          # Call the periodic_reading function with the values
+        readings=return_by_upload()         
         return render_template('index.html', data=readings)
     except subprocess.CalledProcessError as e:
         print("Error running request:", e)
@@ -86,7 +101,7 @@ def view_by_upload():
 def run_dated():
     try:    
         time_str = str(request.form['time'])
-        reading_at(time_str)          # Call the periodic_reading function with the values
+        reading_at(time_str)
     except subprocess.CalledProcessError as e:
         print("Error running request:", e)
     return redirect(url_for('index'))
