0bc3a5feb8dcc13ac28c94bacc44b3fbc2cc5a08
diegopimi <92610979+diegopimi@users.noreply.github.com>
2024-11-10 10:23:37 -0600

    Updated dependancies

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-import subprocess
-import re
-import datetime
-from pymongo import MongoClient
-import crudFunc
-from functionalities import repeat_function, main_func
+from requester import repeat_function, main_func
 
-# Replace 'localhost' with the actual hostname or IP address of your MongoDB server.
-# If your MongoDB server is running on the default port (27017), you don't need to specify it.
-client = MongoClient('localhost', 27017)
-
-# Replace 'your_database_name' with the name of your existing database.
-db = client['DBPimi']
-
-# Call the repeat_function with my_function and a frequency of 2 seconds
 repeat_function(main_func, 1)
