d7944c2336c20b1f179227a3e46014ce9e4419cc
diegopimi <92610979+diegopimi@users.noreply.github.com>
2024-11-16 20:12:12 -0600

    Read json log instead of mongo db

--- WiGraph/plot.py
+++ WiGraph/plot.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-import subprocess
+from flask import Flask
 import sched
 import time
 from datetime import datetime
-from functionalities import periodic_reading, return_reading, return_all, reading_at, scheduler
+from file_manager import log_file_path
 import plotly.graph_objs as go
-import plotly.io as pio
+import json
 
 scheduler = sched.scheduler(time.time, time.sleep)
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/DBPimi'
-mongo = PyMongo(app)
 
 def graph_download():
-    # Query MongoDB to get the data, sorted by ascending time values
-    cursor = mongo.db.WiSpeed.find({}, {'_id': 0, 'Time': 1, 'Date': 1, 'Download': 1}).sort('Time', 1)
     
-    # Extract x and y values from the query result
+    with open(log_file_path, 'r') as f:
+        data = json.load(f)
+    sorted_data = sorted(data, key=lambda x: x['Time'])
+
+    cursor = [{
+        'Time': item['Time'],
+        'Date': item['Date'],
+        'Download': float(item['Download']),  
+        'Upload': float(item['Upload'])       
+    } for item in sorted_data]
+        
     x_values = []
     y_values = []
     for document in cursor:
@@ -30,16 +34,13 @@ def graph_download():
         x_values.append(datetime_obj)
         y_values.append(document['Download'])
 
-    # Sort the x-values (datetime objects) chronologically
     sorted_indices = sorted(range(len(x_values)), key=lambda i: x_values[i])
     x_values = [x_values[i] for i in sorted_indices]
     y_values = [y_values[i] for i in sorted_indices]
 
-    # Create a Plotly line plot
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x_values, y=y_values, mode='markers', name='Download'))  # Add legend for the line
 
-    # Update layout to include legend
     fig.update_layout(
         title='Download Readings',
         xaxis_title='Date and Time',
@@ -47,16 +48,23 @@ def graph_download():
         legend=dict(title='Legend', orientation='h', yanchor='top', y=1.1, xanchor='center', x=0.5)  # Adjust legend position
     )
     
-    # Convert the Plotly figure to HTML
     html_output = fig.to_html(full_html=False)
     
     return html_output
 
 def graph_upload():
-    # Query MongoDB to get the data, sorted by ascending time values
-    cursor = mongo.db.WiSpeed.find({}, {'_id': 0, 'Time': 1, 'Date': 1, 'Upload': 1}).sort('Time', 1)
-    
-    # Extract x and y values from the query result
+    with open(log_file_path, 'r') as f:
+        data = json.load(f)
+   
+    sorted_data = sorted(data, key=lambda x: x['Time'])
+
+    cursor = [{
+        'Time': item['Time'],
+        'Date': item['Date'],
+        'Download': float(item['Download']),  
+        'Upload': float(item['Upload'])       
+    } for item in sorted_data]
+        
     x_values = []
     y_values = []
     for document in cursor:
@@ -69,16 +77,13 @@ def graph_upload():
         x_values.append(datetime_obj)
         y_values.append(document['Upload'])
 
-    # Sort the x-values (datetime objects) chronologically
     sorted_indices = sorted(range(len(x_values)), key=lambda i: x_values[i])
     x_values = [x_values[i] for i in sorted_indices]
     y_values = [y_values[i] for i in sorted_indices]
 
-    # Create a Plotly line plot
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x_values, y=y_values, mode='markers', name='Upload'))  # Add legend for the line
 
-    # Update layout to include legend
     fig.update_layout(
         title='Upload Readings',
         xaxis_title='Date and Time',
@@ -86,7 +91,6 @@ def graph_upload():
         legend=dict(title='Legend', orientation='h', yanchor='top', y=1.1, xanchor='center', x=0.5)  # Adjust legend position
     )
     
-    # Convert the Plotly figure to HTML
     html_output = fig.to_html(full_html=False)
     
     return html_output
